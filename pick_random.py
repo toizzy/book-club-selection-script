791f0fc8d68b0832b75f3263d39b7619feac371d
Isabel <isabelpapad@gmail.com>
2025-01-13 13:49:56 -0500

    Random selection script

---
+++ pick_random.py
@@ -0,0 +1,48 @@
+import argparse
+import random
+
+already_done_books = []
+already_done_movies = []
+
+# Lists are formed by calling range() on the values of each list.
+book_lists = {"NYT21st_lessthan500": (30,),
+              "booker": (1969, 2024),
+              "booker-international": (2005, 2024, 2)}
+
+# There are 1256 movies in criterion, but so it's I guess more balanced we
+# pick a random stream of 100 of them
+criterion_start = random.randint(0, 1156)
+print(criterion_start)
+movie_lists = {"bfi": (100,), 
+               "criterion": (criterion_start, criterion_start + 100),
+               "variety": (100,)}
+
+def pick_random(media_type, number):
+    if media_type == "book":
+        lists = book_lists
+        done_list = already_done_books
+    elif media_type == "movie":
+        lists = movie_lists
+        done_list = already_done_movies
+
+    full_list = []
+    for list_name, numbers in lists.items():
+        full_list.extend([f"{list_name}-{i}" for i in range(*numbers)])
+    for done_thing in done_list:
+        if done_thing in full_list:
+            full_list.remove(done_thing)
+    choices = random.choices(full_list, k=number)
+    return choices
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Pick next book club!')
+    parser.add_argument("--media", choices = ["book", "movie"], required=True)
+    parser.add_argument("--how-many", type=int, required=True)
+    args = parser.parse_args()
+
+    choices = pick_random(args.media, args.how_many)
+    print(choices)
+
+if __name__ == "__main__":
+    main()
